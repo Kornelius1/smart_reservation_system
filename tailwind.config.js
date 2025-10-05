5ccb3f991c6da1cdb02553ddc733e7c58082a690
Mhd Aqsal Maulana <fahiraputri585@gmail.com>
2025-10-05 22:29:19 +0700

    Menyelesaikan halaman menu versi 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-        './resources/js/**/*.js', // Pastikan path JS ada di sini
+        './resources/js/**/*.js',
     ],
 
     theme: {
@@ -29,4 +28,36 @@ export default {
         forms,
         require('daisyui'),
     ],
+
+    // Konfigurasi DaisyUI dengan tema light yang sudah di-custom
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    "primary": "#570df8",
+                    "primary-content": "#ffffff",
+                    "secondary": "#f000b8",
+                    "secondary-content": "#ffffff",
+                    "accent": "#37cdbe",
+                    "accent-content": "#163835",
+                    "neutral": "#3d4451",
+                    "neutral-content": "#ffffff",
+                    "base-100": "#ffffff",
+                    "base-200": "#F2F2F2",
+                    "base-300": "#E5E6E6",
+                    "base-content": "#1f2937",
+                    "info": "#3abff8",
+                    "info-content": "#002b3d",
+                    "warning": "#fbbd23",
+                    "warning-content": "#382800",
+                    "error": "#f87272",
+                    "error-content": "#470000",
+                    
+                    // Warna custom-mu yang menimpa warna 'success' default
+                    "success": "#9CAF88",
+                    "success-content": "#ffffff",
+                },
+            },
+        ],
+    },
 };
