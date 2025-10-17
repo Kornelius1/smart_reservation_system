63a562c9a869cd1a659b8773a214a2def26d77b6
unknown <korneliusjonathan31@gmail.com>
2025-10-17 14:50:05 +0700

    fix: Memperbaiki App.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,15 +4,10 @@ import Alpine from "alpinejs";
 import "../css/app.css";
 import "../css/component.css";
 
-import MenuManagement from "./manajemen-menu.js";
-import PesanMenu from "./PesanMenu.vue";
-
-const app = createApp({});
-
-app.component("manajemen-menu", MenuManagement);
-app.component("pesan-menu", PesanMenu);
-
-app.mount("#app");
-
 window.Alpine = Alpine;
 Alpine.start();
+
+import PesanMenu from "./PesanMenu.vue";
+// import MenuManagement from "./manajemen-menu.js";
+createApp(PesanMenu).mount("#pesan-menu");
+// createApp(MenuManagement).mount("#manajemen-menu");
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,12 +1,9 @@
-{{-- File: resources/views/layouts/guest.blade.php --}}
-
 <!DOCTYPE html>
 <html lang="id">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- Judul halaman bisa dinamis --}}
     <title>@yield('title', 'Homey Cafe')</title>
     <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/app.css', 'resources/css/reservasi.css', 'resources/js/app.js'])
@@ -15,11 +12,10 @@
 
 <body class="body-custom min-h-screen flex flex-col">
 
-    {{-- Header bisa dibuat dinamis juga jika perlu, atau dibiarkan statis --}}
+
     @yield('header')
 
     <main class="flex-grow flex items-center justify-center p-6">
-        {{-- Di sinilah konten unik dari setiap halaman akan ditampilkan --}}
         @yield('content')
     </main>
 
