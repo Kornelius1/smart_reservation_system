6f27f9a5a07747e2a726efa29b7afc5ac65afb40
unknown <korneliusjonathan31@gmail.com>
2025-11-23 20:32:20 +0700

    perbaiki struk yang dikirim lewat email

--- app/Mail/PaymentReceiptMail.php
+++ app/Mail/PaymentReceiptMail.php
@@ -27,26 +27,38 @@ class PaymentReceiptMail extends Mailable
      */
     public function build()
     {
-        // 1. Data Statis Toko (Bisa kamu ganti atau ambil dari Config/DB)
+        $height_base = 240; 
+        
+        $height_per_item = 15; 
+
+        $total_items = $this->reservation->products->count();
+
+        $buffer = 10;
+
+        $total_height = $height_base + ($total_items * $height_per_item) + $buffer;
+
+        $customPaper = [0, 0, 227, $total_height];
+
         $shopData = [
             'reservation' => $this->reservation,
             'app_name'    => 'HOMEY CAFE',
-            'alamat'      => 'Jl. Hr. Soebrantas, Panam, Pekanbaru',
+            'alamat'      => 'Jl. Mawar, Simpang Baru, Kec. Tampan, Kota Pekanbaru, Riau 28293',
             'ig'          => '@homeycafe.pku',
         ];
+ 
+        $pdf = Pdf::loadView('receipts.pdf', $shopData)
+            ->setPaper($customPaper, 'portrait') 
+            ->setOptions([
+                'defaultFont' => 'Courier',
+                'isHtml5ParserEnabled' => true,
+                'isRemoteEnabled' => true,
+                'dpi' => 72,
+            ]);
 
-        // 2. Load View PDF dengan data di atas
-        // Pastikan ukuran kertas diset sesuai kebutuhan (custom thermal size atau A4)
-        // Di sini saya set auto/custom sesuai CSS kamu (@page margin 0)
-        $pdf = Pdf::loadView('receipts.pdf', $shopData) 
-                ->setPaper([0, 0, 226.77, 600], 'portrait');
-
-        // 3. Render Email
-        return $this->subject('E-Struk Pembayaran - Homey Cafe')
-                    ->view('emails.payment_receipt_body') // Body email (bukan lampiran)
-                    ->with(['reservation' => $this->reservation])
-                    ->attachData($pdf->output(), 'Struk-' . $this->reservation->id_transaksi . '.pdf', [
-                        'mime' => 'application/pdf',
-                    ]);
+        return $this->subject('Struk Pembayaran - Homey Cafe')
+                        ->view('emails.payment_receipt_body')
+                        ->attachData($pdf->output(), 'Struk-' . $this->reservation->id_transaksi . '.pdf', [
+                            'mime' => 'application/pdf',
+                        ]);
     }
 }
